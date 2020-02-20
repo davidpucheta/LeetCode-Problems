c0a50653bc9220f943ab83caeb21fd637617cdb0
David Reyes-Pucheta <davidpucheta@gmail.com>
2020-02-20 11:48:42 -0600

    Solve 1.TwoSum problem

---
+++ LeetCode/Problems/TwoSumIndices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    class TwoSumIndices
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var result = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i != j && nums[i] + nums[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
