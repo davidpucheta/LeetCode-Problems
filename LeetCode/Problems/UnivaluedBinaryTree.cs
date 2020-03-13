1c08f2f8a09496277615b1b75ec25bcb7e7a572e
David Reyes-Pucheta <davidpucheta@gmail.com>
2020-03-13 17:37:54 -0600

    Accepted solution for 965. Univalued Binary Tree

---
+++ LeetCode/Problems/UnivaluedBinaryTree.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    public class UnivaluedBinaryTree
+    {
+        public bool IsUnivalTree(TreeNode root)
+        {
+            
+            
+            if (root == null)
+            {
+                return false;
+            }
+            
+            var toReturn = false;
+            var value = root.val;
+            var nodeQueue = new Queue<TreeNode>();
+            nodeQueue.Enqueue(root);
+
+            while (nodeQueue.Count > 0)
+            {
+                var tempNode = nodeQueue.Dequeue();
+                
+                if (tempNode.val != value)
+                {
+                    return false;
+                }
+
+                toReturn = true;
+                
+                if (tempNode.left != null)
+                {
+                    nodeQueue.Enqueue(tempNode.left);                     
+                }
+                if (tempNode.right != null)
+                {
+                    nodeQueue.Enqueue(tempNode.right);                     
+                }
+            }
+            
+            return toReturn;
+        }
+    }
+}
