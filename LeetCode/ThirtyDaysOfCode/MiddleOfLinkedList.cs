e164d435ce1d8e227f08636dd1b4bac228b39ff0
David Reyes-Pucheta <davidpucheta@gmail.com>
2020-04-09 18:53:59 -0500

    Solution for Middle of linked list - 30 days of code

---
+++ LeetCode/ThirtyDaysOfCode/MiddleOfLinkedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.ThirtyDaysOfCode
+{
+    public class MiddleOfLinkedList
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            var slow = head;
+            var fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+}
